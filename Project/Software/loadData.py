2d3c933879fb8e44546a91f13e231de427160277
mapaul <mapaul@users.noreply.github.com>
2019-12-17 20:51:21 +0100

    hurensohn git

--- Project/Software/loadData.py
+++ Project/Software/loadData.py
@@ -45,22 +45,64 @@ class loadData():
         data = self.loadLabel()
         labeldata = []
         for dic in data:
-            bb = [0.0]*4
-            bb[0] = int(dic['2dBoundingBox'][0]*self.dimx)
-            bb[1] = int(dic['2dBoundingBox'][1]*self.dimy)
-            bb[2] = int(dic['2dBoundingBox'][2]*self.dimx)
-            bb[3] = int(dic['2dBoundingBox'][3]*self.dimy)             
+            bb = [0.0]*5
+#            bb[0] = int(dic['2dBoundingBox'][0]*self.dimx)
+#            bb[1] = int(dic['2dBoundingBox'][1]*self.dimy)
+#            bb[2] = int(dic['2dBoundingBox'][2]*self.dimx)
+#            bb[3] = int(dic['2dBoundingBox'][3]*self.dimy)  
+            bb[0] = dic['2dBoundingBox'][0]
+            bb[1] = dic['2dBoundingBox'][1]
+            bb[2] = dic['2dBoundingBox'][2]
+            bb[3] = dic['2dBoundingBox'][3]  
+            bb[4] = int(1)
             labeldata.append(bb)
-        return labeldata
-    
-    #------bearbeiten-----
-    def splitandsafe(self, test_size=0.5):
-        train_features , test_features ,train_labels, test_labels = train_test_split(self.loadImg() , self.loadLabel2D() , test_size)
-        np.save( os.path.join( self.dir , 'x.npy' ) , train_features )
-        np.save( os.path.join( self.dir , 'y.npy' )  , train_labels )
-        np.save( os.path.join( self.dir , 'test_x.npy' ) , test_features )
-        np.save( os.path.join( self.dir , 'test_y.npy' ) , test_labels )
-            
+        
+        return np.array(labeldata)
+        
+
+    def LoadAndSafe2D(self, split_size=0.5, save = False):
+        train_features , test_features ,train_labels, test_labels = train_test_split(self.loadImg() , self.loadLabel2D() , test_size = split_size)
+        if save:
+            np.save( os.path.join( self.dir , 'train_x.npy' ) , train_features )
+            np.save( os.path.join( self.dir , 'train_y.npy' )  , train_labels )
+            np.save( os.path.join( self.dir , 'test_x.npy' ) , test_features )
+            np.save( os.path.join( self.dir , 'test_y.npy' ) , test_labels ) 
+        return np.array(train_features) , np.array(test_features) , np.array(train_labels), np.array(test_labels)
+        
+    def loadLabel3D(self):
+        data = self.loadLabel()
+        labeldata = []
+
+        for dic in data:
+            bb = [0.0]*17
+            bb[0] = int(dic['3dBoundingBox'][0]*self.dimx)
+            bb[1] = int(dic['3dBoundingBox'][1]*self.dimy)
+            bb[2] = int(dic['3dBoundingBox'][2]*self.dimx)        
+            bb[3] = int(dic['3dBoundingBox'][3]*self.dimy)
+            bb[4] = int(dic['3dBoundingBox'][4]*self.dimx)  
+            bb[5] = int(dic['3dBoundingBox'][5]*self.dimy)
+            bb[6] = int(dic['3dBoundingBox'][6]*self.dimx)        
+            bb[7] = int(dic['3dBoundingBox'][7]*self.dimy)
+            bb[8] = int(dic['3dBoundingBox'][8]*self.dimx)
+            bb[9] = int(dic['3dBoundingBox'][9]*self.dimy)
+            bb[10] = int(dic['3dBoundingBox'][10]*self.dimx)        
+            bb[11] = int(dic['3dBoundingBox'][11]*self.dimy)
+            bb[12] = int(dic['3dBoundingBox'][12]*self.dimx)  
+            bb[13] = int(dic['3dBoundingBox'][13]*self.dimy)
+            bb[14] = int(dic['3dBoundingBox'][14]*self.dimx)        
+            bb[15] = int(dic['3dBoundingBox'][15]*self.dimy)            
+            bb[16] = int(1)
+            labeldata.append(bb)
+        return np.array(labeldata)
+        
+    def LoadAndSafe3D(self, split_size=0.5, save = False):
+        train_features , test_features ,train_labels, test_labels = train_test_split(self.loadImg() , self.loadLabel3D() , test_size = split_size)
+        if save:
+            np.save( os.path.join( self.dir , 'train_x.npy' ) , train_features )
+            np.save( os.path.join( self.dir , 'train_y.npy' )  , train_labels )
+            np.save( os.path.join( self.dir , 'test_x.npy' ) , test_features )
+            np.save( os.path.join( self.dir , 'test_y.npy' ) , test_labels ) 
+        return np.array(train_features) , np.array(test_features) , np.array(train_labels), np.array(test_labels)    
             
            
 ld = loadData()
---
+++ Project/Software/object_localizer.py
@@ -0,0 +1,260 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  9 15:18:46 2019
+
+@author: Paul
+"""
+from tensorflow import keras
+
+from tensorflow.keras import backend as K
+import tensorflow as tf
+import time
+#from keras.utils import plot_model
+
+
+#2D Class
+class ObjectLocalizer2D ( object ) :
+
+	def __init__(self, input_shape):
+
+		#tf.logging.set_verbosity(tf.logging.ERROR)
+		alpha = 0.2
+
+		def calculate_iou(target_boxes, pred_boxes):
+			xA = K.maximum(target_boxes[..., 0], pred_boxes[..., 0])
+			yA = K.maximum(target_boxes[..., 1], pred_boxes[..., 1])
+			xB = K.minimum(target_boxes[..., 2], pred_boxes[..., 2])
+			yB = K.minimum(target_boxes[..., 3], pred_boxes[..., 3])
+			interArea = K.maximum(0.0, xB - xA) * K.maximum(0.0, yB - yA)
+			boxAArea = (target_boxes[..., 2] - target_boxes[..., 0]) * (target_boxes[..., 3] - target_boxes[..., 1])
+			boxBArea = (pred_boxes[..., 2] - pred_boxes[..., 0]) * (pred_boxes[..., 3] - pred_boxes[..., 1])
+			iou = interArea / (boxAArea + boxBArea - interArea)
+			return iou
+
+		def custom_loss(y_true, y_pred):
+			mse = tf.losses.mean_squared_error(y_true, y_pred)
+			iou = calculate_iou(y_true, y_pred)
+			return mse + (1 - iou)
+
+		def iou_metric(y_true, y_pred):
+			return calculate_iou(y_true, y_pred)
+
+		model_layers = [
+			keras.layers.Conv2D(16, kernel_size=(3, 3), strides=1, input_shape=input_shape),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(16, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(32, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(32, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(64, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(64, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(128, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(128, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(256, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(256, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Flatten(),
+
+			keras.layers.Dense(1240),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(640),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(480),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(120),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(62),
+			keras.layers.LeakyReLU(alpha=alpha),
+
+			keras.layers.Dense(5),
+			keras.layers.LeakyReLU(alpha=alpha),
+		]
+
+		self.__model = keras.Sequential(model_layers)
+		self.__model.compile(
+			optimizer=keras.optimizers.Adam(lr=0.0001),
+			loss=custom_loss,
+			metrics=[iou_metric]
+		) 
+		#plot_model(self.__model, to_file='model.png')
+    
+
+
+
+	def fit(self, X, Y, hyperparameters):
+		print(Y)           
+		initial_time = time.time()
+		self.__model.fit(X, Y,
+						 batch_size=hyperparameters['batch_size'],
+						 epochs=hyperparameters['epochs'],
+						 callbacks=hyperparameters['callbacks'],
+						 validation_data=hyperparameters['val_data']
+						 )
+		final_time = time.time()
+		eta = (final_time - initial_time)
+		time_unit = 'seconds'
+		if eta >= 60:
+			eta = eta / 60
+			time_unit = 'minutes'
+		self.__model.summary()
+		print('Elapsed time acquired for {} epoch(s) -> {} {}'.format(hyperparameters['epochs'], eta, time_unit))
+
+
+	def evaluate(self, test_X, test_Y):
+		return self.__model.evaluate(test_X, test_Y)
+
+
+	def predict(self, X):
+		predictions = self.__model.predict(X)
+		return predictions
+
+
+	def save_model(self, file_path):
+		self.__model.save(file_path)
+
+
+	def load_model(self, file_path):
+		self.__model = keras.models.load_model(file_path)
+
+	def load_model_weights(self , file_path ) :
+		self.__model.load_weights( file_path )
+        
+#3D Class
+import math
+class ObjectLocalizer3D ( object ) :
+
+	def __init__(self, input_shape):
+		alpha = 0.2
+        
+		def custom_loss(y_true, y_pred):
+			mse = tf.losses.mean_squared_error(y_true, y_pred)			
+			return mse 
+
+		def calculate_iou(target_boxes, pred_boxes):
+			
+			xA = (target_boxes[..., 0] + target_boxes[..., 2] + target_boxes[..., 4] + target_boxes[..., 6] + target_boxes[..., 8] + target_boxes[..., 10] + target_boxes[..., 12] + target_boxes[..., 14])/8
+			yA = (target_boxes[..., 1] + target_boxes[..., 3] + target_boxes[..., 5] + target_boxes[..., 7] + target_boxes[..., 9] + target_boxes[..., 11] + target_boxes[..., 13] + target_boxes[..., 15])/8      
+			xB = (pred_boxes[..., 0] + pred_boxes[..., 2] + pred_boxes[..., 4] + pred_boxes[..., 6] + pred_boxes[..., 8] + pred_boxes[..., 10] + pred_boxes[..., 12] + pred_boxes[..., 14])/8
+			yB = (pred_boxes[..., 1] + pred_boxes[..., 3] + pred_boxes[..., 5] + pred_boxes[..., 7] + pred_boxes[..., 9] + pred_boxes[..., 11] + pred_boxes[..., 13] + pred_boxes[..., 15])/8
+			ret = (((xA-xB)*(xA-xB)+(yA-yB)*(yA-yB)))
+			print(ret)
+			return ret
+       
+        
+		def iou_metric(y_true, y_pred):
+			return calculate_iou(y_true, y_pred)
+        
+		print("Intialize Network")
+
+        
+		model_layers = [
+			keras.layers.Conv2D(16, kernel_size=(3, 3), strides=1, input_shape=input_shape),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(16, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(32, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(32, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(64, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(64, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(128, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(128, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Conv2D(256, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Conv2D(256, kernel_size=(3, 3), strides=1),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+			keras.layers.Flatten(),
+
+			keras.layers.Dense(1240),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(640),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(480),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(120),
+			keras.layers.LeakyReLU(alpha=alpha),
+			keras.layers.Dense(62),
+			keras.layers.LeakyReLU(alpha=alpha),
+
+			keras.layers.Dense( 17 ),
+			keras.layers.LeakyReLU(alpha=alpha),
+		]
+        
+		print("Create Network")
+		self.__model = keras.Sequential(model_layers)
+		print("Compile Network")
+		self.__model.compile(
+			optimizer=keras.optimizers.Adam(lr=0.0001),
+			loss=custom_loss , 
+			metrics=[iou_metric]
+		)
+
+	def fit(self, X, Y, hyperparameters):
+		initial_time = time.time()
+		print(Y)        
+		self.__model.fit(X, Y,
+						 batch_size=hyperparameters['batch_size'],
+						 epochs=hyperparameters['epochs'],
+						 callbacks=hyperparameters['callbacks'],
+						 validation_data=hyperparameters['val_data']
+						 )
+		final_time = time.time()
+		eta = (final_time - initial_time)
+		time_unit = 'seconds'
+		if eta >= 60:
+			eta = eta / 60
+			time_unit = 'minutes'
+		self.__model.summary()
+		print('Elapsed time acquired for {} epoch(s) -> {} {}'.format(hyperparameters['epochs'], eta, time_unit))
+
+
+	def evaluate(self, test_X, test_Y):
+		return self.__model.evaluate(test_X, test_Y)
+
+
+	def predict(self, X):
+		predictions = self.__model.predict(X)
+		return predictions
+
+
+	def save_model(self, file_path):
+		self.__model.save(file_path)
+
+
+	def load_model(self, file_path):
+		self.__model = keras.models.load_model(file_path)
+
+	def load_model_weights(self , file_path ) :
+		self.__model.load_weights( file_path )        
